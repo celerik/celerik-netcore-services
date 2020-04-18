5cc183415a4e87f9a8adc8885d9b8be830e8f345
Juan Cuartas <equisoide@gmail.com>
2020-04-18 00:53:53 -0500

    Created ServiceBaseTest class

---
+++ source/Celerik.NetCore.Services.Test/ServiceBaseTest.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Celerik.NetCore.Util;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celerik.NetCore.Services.Test
+{
+    public class ServiceBaseTest : BaseTest
+    {
+        protected override void AddServices(IServiceCollection services)
+        {
+            services.AddBaseServices();
+            services.AddSingleton(new MapperConfiguration(c => { }).CreateMapper());
+            services.AddTransient<ApiServiceArgs<ServiceBaseTest>>();
+            services.AddTransient<ICalculatorService, CalculatorService>();
+            services.AddTransient<IValidator<PaginationRequest>, PaginationRequestValidator<PaginationRequest>>();
+        }
+    }
+}

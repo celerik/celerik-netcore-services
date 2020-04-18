ef1d0ad8e16a79793666d188209b32e93044946d
Juan Cuartas <equisoide@gmail.com>
2020-04-18 00:19:11 -0500

    Added ApiResponse class without Data

--- source/Celerik.NetCore.Services/Model/ApiResponse.cs
+++ source/Celerik.NetCore.Services/Model/ApiResponse.cs
@@ -1,8 +1,31 @@
-﻿using Celerik.NetCore.Util;
+﻿using System.Diagnostics.CodeAnalysis;
+using Celerik.NetCore.Util;
 using Newtonsoft.Json;
 
 namespace Celerik.NetCore.Services
 {
+    /// <summary>
+    /// Defines the standarized response for all services.
+    /// </summary>
+    public class ApiResponse
+    {
+        /// <summary>
+        /// Optional message in case something happened during the service execution.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Describes the type of message.
+        /// </summary>
+        [JsonConverter(typeof(EnumDescriptionJsonConverter))]
+        public ApiMessageType? MessageType { get; set; }
+
+        /// <summary>
+        /// Indicates whether the service was successfully executed.
+        /// </summary>
+        public bool Success { get; set; }
+    }
+
     /// <summary>
     /// Defines the standarized response for all services.
     /// </summary>
@@ -14,9 +37,7 @@ namespace Celerik.NetCore.Services
         /// </summary>
         /// <param name="data">Data sent as the response.</param>
         public ApiResponse(TData data = default)
-        {
-            Data = data;
-        }
+            => Data = data;
 
         /// <summary>
         /// Data sent as the response.
@@ -44,5 +65,20 @@ namespace Celerik.NetCore.Services
         /// </summary>
         /// <returns>JSON string that represents the current object.</returns>
         public override string ToString() => JsonConvert.SerializeObject(this);
+
+
+        /// <summary>
+        /// Converts an ApiResponse to an ApiResponse&lt;TData&gt;.
+        /// </summary>
+        /// <param name="response">The object to cast.</param>
+        [SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Implicit operators should not throw exceptions")]
+        public static implicit operator ApiResponse<TData>(ApiResponse response)
+            => new ApiResponse<TData>
+            {
+                Data = default,
+                Message = response.Message,
+                MessageType = response.MessageType,
+                Success = response.Success
+            };
     }
 }

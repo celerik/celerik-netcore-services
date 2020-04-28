7d81315bade54e6f33af96a5cc88b53c43d038ca
Juan Cuartas <equisoide@gmail.com>
2020-04-28 12:26:07 -0500

    Created ApiError model

---
+++ source/Celerik.NetCore.Services/Model/ApiError.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Celerik.NetCore.Services
+{
+    /// <summary>
+    /// Defines the standard response for all errors.
+    /// </summary>
+    /// <typeparam name="TStatusCode">Type of the StatusCode property.
+    /// </typeparam>
+    public class ApiError<TStatusCode>
+         where TStatusCode : struct, IConvertible
+    {
+        /// <summary>
+        /// A localized string describing the error.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The status code related to this error (enumeration).
+        /// </summary>
+        public TStatusCode StatusCode { get; set; }
+    }
+}

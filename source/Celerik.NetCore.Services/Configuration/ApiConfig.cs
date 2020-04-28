54e0cd015010d29e5745cb4c695e0c1678d8b9e9
Juan Cuartas <equisoide@gmail.com>
2020-04-28 12:24:17 -0500

    Added ApiConfig class

---
+++ source/Celerik.NetCore.Services/Configuration/ApiConfig.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Celerik.NetCore.Services
+{
+    /// <summary>
+    /// Defines common configuration to add core services.
+    /// </summary>
+    public class ApiConfig
+    {
+        /// <summary>
+        /// Provides programmatic configuration for localization.
+        /// </summary>
+        public LocalizationOptions LocalizationOptions { get; set; }
+            = new LocalizationOptions
+            {
+                ResourcesPath = "Resources"
+            };
+
+        /// <summary>
+        /// Provides programmatic configuration for the Console logger.
+        /// </summary>
+        public ConsoleLoggerOptions ConsoleLoggerOptions { get; set; }
+            = new ConsoleLoggerOptions
+            {
+                TimestampFormat = "[yyyy-MM-dd HH:mm:ss]"
+            };
+
+        /// <summary>
+        /// Represents all the options you can use to configure the
+        /// identity system.
+        /// </summary>
+        public IdentityOptions IdentityOptions { get; set; }
+            = new IdentityOptions
+            {
+                User = new UserOptions
+                {
+                    AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$^+=!*()@%&",
+                    RequireUniqueEmail = true
+                },
+                Password = new PasswordOptions
+                {
+                    RequiredLength = 8
+                },
+                SignIn = new SignInOptions
+                {
+                    RequireConfirmedEmail = true,
+                    RequireConfirmedAccount = true
+                }
+            };
+    }
+}

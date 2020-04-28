778e0ae60fdb5a845fd00e13614092b775b45a6b
Juan Cuartas <equisoide@gmail.com>
2020-04-28 12:24:47 -0500

    Created ApiChangeAction enumeration

---
+++ source/Celerik.NetCore.Services/Enumerations/ApiChangeAction.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Celerik.NetCore.Services
+{
+    /// <summary>
+    /// Defines the type of change a entity will undergo when changes
+    /// are submitted to the database.
+    /// </summary>
+    public enum ApiChangeAction
+    {
+        /// <summary>
+        /// The entity will be inserted.
+        /// </summary>
+        [Description("Insert")]
+        Insert = 1,
+
+        /// <summary>
+        /// The entity will be updated.
+        /// </summary>
+        [Description("Update")]
+        Update = 2,
+
+        /// <summary>
+        /// The entity will be deleted.
+        /// </summary>
+        [Description("Delete")]
+        Delete = 3,
+    }
+}

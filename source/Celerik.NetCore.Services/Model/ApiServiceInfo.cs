ddd06714471135cc75c7fbe829024d52dbf59f4f
AzureAD\JuanDavidCuartas <jcuartas@celerik.com>
2022-11-24 14:50:13 -0500

    Added ApiServiceInfo

---
+++ source/Celerik.NetCore.Services/Model/ApiServiceInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Celerik.NetCore.Services
+{
+    /// <summary>
+    /// Provides service information.
+    /// </summary>
+    public class ApiServiceInfo
+    {
+        /// <summary>
+        /// The service name. E.g: "MasterDataSVC".
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The service version. E.g: "0.1.1"
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
